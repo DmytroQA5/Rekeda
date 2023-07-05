dbcbe97644b49d10b69c92f1a41491d0328abe1f
Rekeda <rekeda1990@gmail.com>
2023-07-05 19:08:39 +0300

    123

--- src/home1/Task4.java
+++ src/home1/Task4.java
@@ -2,6 +2,13 @@ package home1;
 
 public class Task4 {
     public static void main(String[] args) {
-
+        int a = 7;
+        int b = 9;
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
     }
 }
