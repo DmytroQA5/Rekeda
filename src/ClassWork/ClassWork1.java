76275aac407b264f62f9112b1b7cf61c31aaf9eb
Rekeda <rekeda1990@gmail.com>
2023-07-06 19:24:16 +0300

    4555

---
+++ src/ClassWork/ClassWork1.java
@@ -0,0 +1,12 @@
+package ClassWork;
+
+import java.util.Scanner;
+
+public class ClassWork1 {
+    public static void main(String[] args) {
+        Scanner dls = new Scanner(System.in);
+        System.out.println("Insert");
+        int x = dls.nextInt();
+        if(x % 12 == 0 &&)
+    }
+}
