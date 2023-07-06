ca1f72b7900a1e3755d0f1e67b21952b35bb7016
Rekeda <rekeda1990@gmail.com>
2023-07-06 19:41:05 +0300

    ДЗ 5.1

--- src/home2/Task5.java
+++ src/home2/Task5.java
@@ -3,8 +3,8 @@ package home2;
 public class Task5 {
     public static void main(String[] args) {
         String string1 = "This line that i want to cut, cause it is too long";
-        String string2 = string1.substring(0, 34);
-        String string3 = string2 + " This line that don't want to cut, cause it is perfect";
+        String string2 = string1.substring(0,34);
+        String string3 = string2 + " this line that don't want to cut, cause it is perfect";
 
         System.out.println("string2: " + string2 + ", длина: " + string2.length());
         System.out.println("string3: " + string3 + ", длина: " + string3.length());
