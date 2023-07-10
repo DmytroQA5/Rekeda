c36d09377ae33f00c75bd90807738f39504369cd
Rekeda <rekeda1990@gmail.com>
2023-07-10 19:55:22 +0300

    wewr

--- src/ClassWork/Class1.java
+++ src/ClassWork/Class1.java
@@ -1,4 +1,30 @@
 package ClassWork;
 
+import java.util.Scanner;
+
 public class Class1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            System.out.print("Введите число (или введите 'FINAL' для завершения): ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("FINAL")) {
+                break;
+            }
+
+            try {
+                int number = Integer.parseInt(input);
+                sum += number;
+            } catch (NumberFormatException e) {
+                System.out.println("Некорректный ввод. Пожалуйста, введите число или 'FINAL'.");
+            }
+        }
+
+        System.out.println("Конечная сумма: " + sum);
+    }
 }
+
+
